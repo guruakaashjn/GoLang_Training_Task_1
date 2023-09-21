af7d9b714b76634385ce8cf6cf0777cf683ba71f
guruakaashjn <gurujnn@gmail.com>
2023-09-21 19:24:28 +0530

    Update TicTacToe Implementation Code

--- Facade_Pattern/tictactoeupdated/game/game.go
+++ Facade_Pattern/tictactoeupdated/game/game.go
@@ -1,7 +1,6 @@
 package game
 
 import (
-	"fmt"
 	"tictactoeupdated/board"
 	"tictactoeupdated/errors"
 	"tictactoeupdated/player"
@@ -28,65 +27,84 @@ func NewGame(player0Name, player1Name string) *Game {
 	}
 }
 
-func (g *Game) Play() {
-	for i := 0; i < 1; {
-	startturn:
-
-		g.board.PrintBoard()
-
-		if g.isGameEnded {
-			fmt.Println(g.result)
-			i++
-		} else {
-			fmt.Printf("Turn %d\n", g.turn)
-			fmt.Println("Enter cell number: ")
-			var cellNumber uint
-			fmt.Scan(&cellNumber)
-			var flag, response = g.PlayLogic(cellNumber)
-			if flag {
-				g.board.PrintBoard()
-				fmt.Println(response)
-				g.isGameEnded = true
-				i++
-				continue
-			}
-			if !flag {
-				if response == "Cell Not Empty" {
-					fmt.Println(response)
-					goto startturn
-				} else {
-					g.turn++
-					fmt.Println(response)
-				}
-
-			}
-		}
-
-	}
+func (g *Game) GetTurn() uint {
+	return g.turn
 }
 
+func (g *Game) SetTurn() {
+	g.turn++
+}
+
+// func (g *Game) Play() {
+// 	for i := 0; i < 1; {
+// 	startturn:
+
+// 		g.board.PrintBoard()
+
+// 		if !g.isGameEnded {
+// 			fmt.Printf("Turn %d\n", g.turn)
+// 			fmt.Println("Enter cell number: ")
+// 			var cellNumber uint
+// 			fmt.Scan(&cellNumber)
+// 			var flag, response = g.PlayLogic(cellNumber)
+// 			if flag {
+// 				g.board.PrintBoard()
+// 				fmt.Println(response)
+// 				g.isGameEnded = true
+// 				i++
+// 				continue
+// 			}
+// 			if !flag {
+// 				if response == "Cell Not Empty" || response == "Cell Index Out of Bounds" {
+// 					fmt.Println(response)
+// 					goto startturn
+// 				} else {
+// 					g.turn++
+// 					fmt.Println(response)
+// 				}
+
+// 			}
+
+// 		} else {
+// 			fmt.Println(g.result)
+// 			i++
+// 		}
+
+// 	}
+// }
+
 func (g *Game) PlayLogic(cellNumber uint) (flag bool, response string) {
+
 	// var response string
 	defer func() {
+		g.board.PrintBoard()
 		if a := recover(); a != nil {
 
 			flag = false
 			response = a.(string)
 			// fmt.Println("Recovered", response)
+			if !(response == "Cell Not Empty" || response == "Cell Index Out of Bounds") {
+				g.SetTurn()
+			}
 
 		}
 	}()
 
-	flag, response = g.PlayLogicInner(cellNumber)
-	if flag {
-		// fmt.Println(response)
+	if g.isGameEnded {
 		flag = true
+		response = g.result
+	} else {
+		flag, response = g.PlayLogicInner(cellNumber)
 	}
+
 	return flag, response
 
 }
 
 func (g *Game) PlayLogicInner(cellNumber uint) (bool, string) {
+	if cellNumber >= 9 {
+		panic(errors.NewInvalidMove("Cell Index Out of Bounds").GetSpecificMessage())
+	}
 	if !g.board.IsEmpty(cellNumber) {
 		panic(errors.NewInvalidMove("Cell Not Empty").GetSpecificMessage())
 	}
--- Facade_Pattern/tictactoeupdated/main.go
+++ Facade_Pattern/tictactoeupdated/main.go
@@ -16,9 +16,26 @@ func main() {
 	fmt.Scan(&user2)
 	g1 := game.NewGame(user1, user2)
 
-	g1.Play()
-	fmt.Println("Game ended")
-	g1.Play()
-	g1.Play()
+	// g1.Play()
+	// fmt.Println("Game ended")
+	// g1.Play()
+	// g1.Play()
 
+	for i := 0; i < 1; {
+		// startturn:
+
+		fmt.Printf("\nTurn %d\n", g1.GetTurn())
+		fmt.Printf("Enter cell number: ")
+		var cellNumber uint
+		fmt.Scan(&cellNumber)
+		var flag, response = g1.PlayLogic(cellNumber)
+		if flag {
+			i++
+		}
+		fmt.Println(response)
+	}
+
+	var flag, response = g1.PlayLogic(1)
+	fmt.Println("F : ", flag)
+	fmt.Println("Response: ", response)
 }
